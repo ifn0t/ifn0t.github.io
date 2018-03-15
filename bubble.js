031f91d2ca95ed4c45fec6a060ab5a50efaeab6a
John Panayiotou <bachprevails@yahoo.com>
2018-03-15 19:57:20 -0400

    Initial upload

---
+++ bubble.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+  var bubbleChart = new d3.svg.BubbleChart({
+    supportResponsive: true,
+    //container: => use @default
+    size: 600,
+    //viewBoxSize: => use @default
+    innerRadius: 600 / 3.5,
+    //outerRadius: => use @default
+    radiusMin: 50,
+    //radiusMax: use @default
+    //intersectDelta: use @default
+    //intersectInc: use @default
+    //circleColor: use @default
+    data: {
+      items: [
+        {text: "12+ years playing classical guitar", count: "236"},
+        {text: "Real-world gigging experience", count: "382"},
+        {text: "Music theory knowledge", count: "170"},
+        {text: "Composer and cover artist", count: "123"},
+      ],
+      eval: function (item) {return item.count;},
+      classed: function (item) {return item.text.split(" ").join("");}
+    },
+    plugins: [
+     /* {
+        name: "central-click",
+        options: {
+         // text: "(See more detail)",
+          style: {
+            "font-size": "12px",
+            "font-style": "italic",
+            "font-family": "Source Sans Pro, sans-serif",
+            //"font-weight": "700",
+            "text-anchor": "middle",
+            "fill": "white"
+          },
+          attr: {dy: "65px"},
+          centralClick: function() {
+          alert("Here is more details!!");
+          }
+        }
+      },*/
+      {
+        name: "lines",
+        options: {
+          format: [
+            {// Line #0
+             // textField: "count",
+              classed: {count: true},
+              style: {
+                "font-size": "28px",
+                "font-family": "Helvetica, Arial, sans-serif",
+                "text-anchor": "middle",
+                fill: "white"
+              },
+              attr: {
+                dy: "0px",
+                x: function (d) {return d.cx;},
+                y: function (d) {return d.cy;}
+              }
+            },
+            {// Line #1
+              textField: "text",
+              classed: {text: true},
+              style: {
+                "font-size": "9px",
+                "font-family": "Helvetica, Arial, sans-serif",
+                "text-anchor": "middle",
+                fill: "white"
+              },
+              attr: {
+                dy: "5px",
+                x: function (d) {return d.cx;},
+                y: function (d) {return d.cy;}
+              }
+            }
+          ],
+          centralFormat: [
+            {// Line #0
+              style: {"font-size": "22px"},
+              attr: {}
+            },
+            {// Line #1
+              style: {"font-size": "22px"},
+              attr: {dy: "5px"}
+            }
+          ]
+        }
+      }]
+  });
+});
